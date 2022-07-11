7ef89417f9fadd0e32159908f3bbfd584be41a48
Lindsey Kelly <nightbirdphoto@gmail.com>
2022-07-10 21:29:59 -0400

    resume pdf

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -4,6 +4,7 @@ function Resume() {
     return (
         <section>
             <h1>My Resume</h1>
+            <link href="src\assets\large\Lindsey Kelly Web Developer.pdf"/>
  {/*link to downloadable resume and a list of the developers proficiencies */}
         </section>
     )
