fba11e9d9c702867660d7e90ddfa0abc1bcf2498
Lindsey Kelly <nightbirdphoto@gmail.com>
2022-07-12 20:46:10 -0400

    undo last changes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,7 +7,7 @@ import React from 'react';
 function Footer() {
 
     return(
-    <footer className='fixed-bottom'>
+    <footer className='footer fixed-bottom'>
         <a href='https://www.linkedin.com/in/lindsey-kelly-ab47b47b/'><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
         <a href='https://github.com/ElleAK'><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" className="icon"/></a>
     </footer>
